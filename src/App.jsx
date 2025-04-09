6a6cfe01d05aaebee03832471951dff5dcca09bc
YuriiDumitriuk <yurii.dumitriuk@gmail.com>
2025-04-09 10:29:59 +0200

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import Lesson from "./pages/Lesson";
+import AudioWord from './pages/AudioWord';
+
+function App() {
+    return (
+        <Router>
+            <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/audio-word" element={<AudioWord />} />
+                {/* Przekazujemy topic jako parametr w URL */}
+                {/*<Route path="/lesson/:topic" element={<Lesson />} />*/}
+            </Routes>
+        </Router>
+    );
+}
+
+export default App;
