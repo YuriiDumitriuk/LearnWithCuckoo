cabda5f38cde4f4d8074ea4e0fbcfd474117fb20
YuriiDumitriuk <yurii.dumitriuk@gmail.com>
2025-04-09 21:23:06 +0200

    config

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/learnwithcuckoo/', // Zmienna bazowa do GitHub Pages (nazwa repozytorium)
+    base: '/LearnWithCuckoo/', // Zmienna bazowa do GitHub Pages (nazwa repozytorium)
 });
