0b0b26314cdbd292dce2bd3ac13329bdc161cde0
YuriiDumitriuk <yurii.dumitriuk@gmail.com>
2025-04-09 20:45:19 +0200

    modified config

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import AudioWord from './pages/AudioWord';
 
 function App() {
     return (
-        <Router>
+        <Router basename={import.meta.env.BASE_URL}>
             <Routes>
                 <Route path="/" element={<Home />} />
                 <Route path="/audio-word" element={<AudioWord />} />
