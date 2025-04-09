ba89c0588857ed489d01baa1ae8c6a6fe0e3f2ed
YuriiDumitriuk <yurii.dumitriuk@gmail.com>
2025-04-09 20:14:18 +0200

    fix deployment

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,11 @@
 import { defineConfig } from 'vite';
-import plugin from '@vitejs/plugin-react';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-    plugins: [plugin()],
+    plugins: [react()],
     server: {
         port: 65362,
-    }
-})
+    },
+    base: '/LearnWithCuckoo', // Zmienna bazowa do GitHub Pages (nazwa repozytorium)
+});
